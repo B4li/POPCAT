f8c8ae0b1991ebfdf825caecf5876ae334219082
B4li <89071531+B4li@users.noreply.github.com>
2021-08-17 19:06:42 +0200

    Create popcatBot.js

---
+++ popcatBot.js
@@ -0,0 +1,14 @@
+# 1 | Allez sur https://popcat.click
+# 2 | Ouvrez la console [F12]
+# 3 | Entrez le code et appuyez sur enter
+
+var event = new KeyboardEvent('keydown', {
+	key: 'g',
+	ctrlKey: true
+});
+
+setInterval(function(){
+	for (i = 0; i < 100; i++) {
+		document.dispatchEvent(event);
+	}
+}, 0);
